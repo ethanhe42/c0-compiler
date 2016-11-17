44442129e922e69b51bee5716df8d3eed3478535
yihuihe <yihuihe@foxmail.com>
2016-11-17 11:08:43 +0800

    q1done

--- q1.sh
+++ q1.sh
@@ -1,8 +1,9 @@
+rm a.out
 flex q1.l
 gcc lex.yy.c
-folder=test
+folder="test"
 for f in ${folder}/*.c ;
 do
     echo "\n" analysing ${f}
-    ./a.out ${f} | tee ${f}.lex.xml ;
+    ./a.out ${f} | tee ${f}.lex.xml
 done
